09f6fb7781cd55f155383c87738bd43459d28898
klasyyi <syarafuddinzaidi@gmail.com>
2026-05-16 16:25:56 +0800

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDLWiR6cC8Meo9BouqswEyWG4tA3otKk88",
+  authDomain: "cyberhax-game.firebaseapp.com",
+  databaseURL: "https://cyberhax-game-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "cyberhax-game",
+  storageBucket: "cyberhax-game.firebasestorage.app",
+  messagingSenderId: "655375801626",
+  appId: "1:655375801626:web:ee2a97b84a3fc01ae560a3"
+};
