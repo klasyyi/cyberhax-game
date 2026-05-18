43ef2c7e253eaf4517c50f1e919af4624116cc6e
klasyyi <syarafuddinzaidi@gmail.com>
2026-05-18 21:33:15 +0800

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,3 +1,6 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-database.js";
+
 const firebaseConfig = {
   apiKey: "AIzaSyDLWiR6cC8Meo9BouqswEyWG4tA3otKk88",
   authDomain: "cyberhax-game.firebaseapp.com",
@@ -7,3 +10,6 @@ const firebaseConfig = {
   messagingSenderId: "655375801626",
   appId: "1:655375801626:web:ee2a97b84a3fc01ae560a3"
 };
+
+const app = initializeApp(firebaseConfig);
+export const db = getDatabase(app);
